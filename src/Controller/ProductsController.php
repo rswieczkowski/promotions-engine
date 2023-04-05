e4b9c67f43109ff59172566e6577d19565e0b999
Rafał Świeczkowski <rswieczkowski@leonsoftware.com>
2023-04-05 19:40:59 +0200

    Routes first commit

---
+++ src/Controller/ProductsController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProductsController extends AbstractController
+{
+    #[Route('/products', name: 'app_products')]
+    public function index(): JsonResponse
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/ProductsController.php',
+        ]);
+    }
+
+    #[Route('/products/{id}/promotions', name: 'promotions', methods: 'GET')]
+    public function promotions()
+    {
+    }
+
+    #[Route('/products/{id}/lowest-price', name: 'lowest-price', methods: 'POST')]
+    public function lowestPrice(Request $request, int $id): Response
+    {
+        if ($request->headers->has('force_fail')) {
+            return new JsonResponse([
+                'error' => 'Promotions Engine failure message'
+            ], $request->headers->get('force_fail'));
+        }
+        return new JsonResponse([
+            'quantity' => 5,
+            'request_location' => "PL",
+            'voucher_code' => "OU812",
+            'request_date' => "2023-04-05",
+            'product_id' => $id,
+            'price' => 100,
+            'discounted_price' => 50,
+            'promotion_id' => 3,
+            'promotion_name' => 'Black Friday half price sale'
+
+
+        ], 200);
+    }
+}
