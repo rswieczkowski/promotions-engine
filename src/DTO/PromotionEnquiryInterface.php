ca0c587abe7c3a3a0fcd4208dc8c3c47f2e311fb
Rafał Świeczkowski <rswieczkowski@leonsoftware.com>
2023-04-26 00:03:57 +0200

    SOLID interface segregation

--- src/DTO/PromotionEnquiryInterface.php
+++ src/DTO/PromotionEnquiryInterface.php
@@ -2,9 +2,16 @@
 
 namespace App\DTO;
 
+use App\Entity\Product;
 use JsonSerializable;
 
 interface PromotionEnquiryInterface
 {
+
+    public function getProduct(): ?Product;
+
+    public function setPromotionId(int $promotionId);
+
+    public function setPromotionName(string $promotionName);
 
 }
